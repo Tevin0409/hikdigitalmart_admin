b70b2eb3298966018e7d33064c36818d52e0b919
Tevin Obai <tevin@nathandigital.com>
2025-02-14 07:38:17 +0300

    changed middleware and added experimental edge

--- src/middleware.ts
+++ src/middleware.ts
@@ -61,7 +61,7 @@ export async function middleware(req: NextRequest) {
       }
     } catch (error) {
       console.error("Failed to refresh token:", error);
-      return NextResponse.redirect(new URL("/login", req.url));
+      return NextResponse.redirect(new URL("/", req.url));
     }
   }
 
